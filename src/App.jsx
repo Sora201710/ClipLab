0d352d0e2ea3f444b8f8f00f72922f23d40c53d8
sora201710 <chrisroybd@gmail.com>
2025-05-21 20:03:37 -0400

    added a background to the site and coloured buttons with icons.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import Button from './components/Button'
+import addIcon from './assets/add.webp'
+import compressIcon from './assets/compress.png'
+import mergeIcon from './assets/merge.webp'
+import trimIcon from './assets/trim.webp'
+
 const App = () => {
   return (
-    <div className='center'>
-      Coda
-      <div className='flex'>
-        <Button text = "+"></Button>
-        <Button text = "Trim"></Button>
-        <Button text = "Merge"></Button>
-        <Button text = "Compress"></Button>
+    <div className="min-h-screen bg-indigo-500 flex items-center justify-center">
+      <div className="bg-gray-100 p-8 rounded-lg shadow-lg text-center">
+        <div className='px-10 flex gap-10 justify-center items-center'>
+          <Button text = "+" icon = {addIcon}></Button>
+          <Button text = "Trim" icon = {trimIcon}></Button>
+          <Button text = "Merge" icon = {mergeIcon}></Button>
+          <Button text = "Compress" icon = {compressIcon}></Button>
+        </div>
       </div>
     </div>
   )
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Button = ({text}) => {
+const Button = ({text, icon}) => {
   return (
-    <button className='font-semibold'>
-        {text}
+    <button className='px-4 w-15 h-15  bg-cyan-500 hover:bg-cyan-600 cursor-pointer p-2 rounded-full flex items-center justify-center focus:outline-none'>
+        <img src={icon} alt={text} />
     </button>
   )
 }
